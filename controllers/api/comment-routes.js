dee572e489a85824fd652f3ba927b8186cde78cc
Theo <trgrf1@gmail.com>
2021-09-21 20:40:03 -0400

    saving my req.body and user id in create method

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,8 +7,9 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       // TODO: COMMENT BODY IN REQUEST USING SPREAD
-
+      ...req.body,
       // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
+      user_id: req.session.user_id
     });
     res.json(newComment);
   } catch (err) {
