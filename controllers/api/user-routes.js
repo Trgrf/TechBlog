0fd43bf050e7018d8577604e3577f5824eba7dc4
Theo <trgrf1@gmail.com>
2021-09-21 20:57:34 -0400

    saving creations of new account

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,8 +6,9 @@ router.post('/', async (req, res) => {
   try {
     const newUser = await User.create({
       // TODO: SET USERNAME TO USERNAME SENT IN REQUEST
-      
+      username = newUser.username,
       // TOD: SET PASSWORD TO PASSWORD SENT IN REQUEST
+      password = newUser.password
     });
 
     req.session.save(() => {
