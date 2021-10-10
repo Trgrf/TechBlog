ebefdbd63dbcd367f1556e68d0d568e0ef01bcce
Theo <trgrf1@gmail.com>
2021-10-10 11:32:43 -0400

    saving login POST route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       // TODO: SET USERID userId IN REQUEST SESSION TO ID RETURNED FROM DATABASE
-      req.session.user_id = user.id,
+      req.session.userId = user.id,
       // TODO: SET USERNAME username IN REQUEST SESSION TO USERNAME RETURNED FROM DATABASE
       req.session.username = user.username,
       // TODO: SET LOGGEDIN loggedIn TO TRUE IN REQUEST SESSION
